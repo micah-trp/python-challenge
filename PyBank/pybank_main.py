5b8cbe8f785bbe0c844b452728a063c2356cc518
Micah Raquena <micahtr@gmail.com>
2023-05-31 22:28:13 +0800

    completed and validated part 1

--- PyBank/pybank_main.py
+++ PyBank/pybank_main.py
@@ -4,6 +4,7 @@ import os
 #Module for reading CSV file
 import csv
 
+
 # from collections import defaultdict
 #-------------------------------------------------------------------------
 # Read in the CSV file
@@ -18,51 +19,52 @@ import csv
 #     for row in pybankreader:
 #         print(row)
 #-------------------------------------------------------------------------
-
- 
+#  
 pybankpath = os.path.join('Resources','budget_data.csv')
 
-# Read the csv and set it up as dictionairies
+# Read the csv and set it up as list dictionary
 with open(pybankpath,encoding='utf') as pybank_csv: 
     pybankreader = csv.reader(pybank_csv,delimiter=',')
 
     # skip first row using next
     next(pybankreader) 
-    revenue = [] #'Provit/Losses'
-    date = []
-    rev_change = []
 
-    # in this loop count total months fromo Date [0] and did sum of revenue (Profit/Losses) [1] 
+    #Set up lists 
+    prof_loss = [] #'Provit/Losses'
+    date = []
+    pl_change = []
+
+    # in this loop count total months from date - column [0] and did sum of prof_loss (Profit/Losses)- column [1] 
     for row in pybankreader:
         date.append(row[0])
-        revenue.append(float(row[1]))   
+        prof_loss.append(float(row[1]))   
 
     print("Financial Analysis")
     print("-----------------------------------")
     print("Total Months:", len(date))
-    print("Total Revenue: $", sum(revenue))
+    print("Total: $", sum(prof_loss))
 
-    #in this loop I did total of difference between all row of column "Revenue" and found total revnue change. Also found out max revenue change and min revenue change. 
-    for i in range(1,len(revenue)):
-        rev_change.append(revenue[i] - revenue[i-1])   
-        avg_rev_change = sum(rev_change)/len(rev_change)
+    #in this loop I did total of difference between all row of column "prof_loss" and found total revnue change. Also found out max prof_loss change and min prof_loss change. 
+    for i in range(1,len(prof_loss)):
+        pl_change.append(prof_loss[i] - prof_loss[i-1])   
+        avg_pl_change = sum(pl_change)/len(pl_change)
 
-        max_rev_change = max(rev_change)
+        max_pl_change = max(pl_change)
 
-        min_rev_change = min(rev_change)
+        min_pl_change = min(pl_change)
 
-        max_rev_change_date = str(date[rev_change.index(max(rev_change))])
-        min_rev_change_date = str(date[rev_change.index(min(rev_change))])
+        max_pl_change_date = str(date[pl_change.index(max(pl_change))])
+        min_pl_change_date = str(date[pl_change.index(min(pl_change))])
 
 
-    print("Avereage Revenue Change: $", round(avg_rev_change))
-    print("Greatest Increase in Revenue:", max_rev_change_date,"($", round(max_rev_change),")")
-    print("Greatest Decrease in Revenue:", min_rev_change_date,"($", round(min_rev_change),")")
+    print("Avereage Change: $", round(avg_pl_change))
+    print("Greatest Increase in Profits:", max_pl_change_date,"($", round(max_pl_change),")")
+    print("Greatest Decrease in Profits:", min_pl_change_date,"($", round(min_pl_change),")")
 
 
 # Validation ---------------------------------------------
 
-# Validation: total months
+# Validation: total months = 86
 # counter = defaultdict(int)
 # with open(pybankpath,encoding='utf') as pybank_csv: 
 #     reader = csv.DictReader(pybank_csv)
@@ -71,7 +73,7 @@ with open(pybankpath,encoding='utf') as pybank_csv:
 # print('Total Months :',(len(dict(counter))))
 
 
-# Validation Total
+# Validation Total $22564198.0
 # with open(pybankpath,encoding='utf') as pybank_csv: 
 #     headerline = next(pybank_csv)
 #     total = 0
@@ -81,3 +83,4 @@ with open(pybankpath,encoding='utf') as pybank_csv:
 
 
 #--------------------------------------------
+
