91319976e41680159ec719a17705f29ed02c3032
Micah Raquena <micahtr@gmail.com>
2023-06-05 16:28:40 +0800

    pypoll loop completed

--- PyPoll/pypoll_main.py
+++ PyPoll/pypoll_main.py
@@ -1,84 +1,78 @@
 #PyBank Challenge Micah Raquena-Pequeno 16.03.23
 #import dependencies
-import os
-#Module for reading CSV file
+import os 
+#Allow us to read csv file
 import csv
 
-#reading in a file
-# pypoll_path = os.path.join('Resources','election_data.csv')
-# with open(pypoll_path,encoding='utf') as pypoll_csv:   
-#     pypoll_reader = csv.reader(pypoll_csv,delimiter=',')
-#     print(pypoll_reader)
-#     pybank_header = next(pypoll_reader)
-#     print(f"CSV Header: {pybank_header}")
-#     for row in pypoll_reader:
-#         print(row)
+#list total_votes
+total_total_votes = []
 
-#-----------------------------------------
+#dictionary for each row
+canidate_total_votes = {}
 
-# The total number of votes cast
-# A complete list of candidates who received votes
-# The percentage of votes each candidate won
-# The total number of votes each candidate won
-# The winner of the election based on popular vote
+#Candidates - using names from csv list.
+Stockham = 0
+Doane = 0
+DeGette = 0
 
-pypollpath = os.path.join('Resources','election_data.csv')
+pypoll_path = os.path.join('Resources','election_data.csv')
 
-#Set up lists 
-balot_id = [] #'Provit/Losses'
-country = []
-candidates = []
-
-
-
-# Read the csv and set it up as list dictionary
-with open(pypollpath,encoding='utf') as pypoll_csv: 
-    pypollreader = csv.reader(pypoll_csv,delimiter=',')
-
-    # skip first row using next
-    next(pypollreader) 
-
-    # read each row into columns/ used excel to confirm candidates. 
-    for row in pypollreader:
-        balot_id.append(row[0])
-        country = [].append(row[1])
-        candidates = [].append(row[2])
-        Stockham = []
-        DeGette = []
-        Doane = []
+#--------------------main loop-----------------
+#read csv file
+with open(pypoll_path) as pypoll_csv:
+    csvreader = csv.reader(pypoll_csv, delimiter=',')
+    #----------skip header row
+    next(csvreader)
     
-    total_votes = len(balot_id)
-     # A complete list of candidates who received votes and the total number of votes each candidate won.
-    if candidates is not None:
-        for row in candidates:
-            if row == "Charles Casper Stockham":
-                Stockham.append(candidates)
-                
-            elif row == "Diana DeGette":
-                DeGette.append(candidates)
-                
-            else:
-                Doane.append(candidates)
-                
-    votes_Stockham = len(Stockham)
-    votes_Degette= len(DeGette)
-    votes_Doane = len(Doane)
-    p_Stockham = round(((votes_Stockham/ total_votes) * 100), 2)
-    p_DeGette = round(((votes_Degette / total_votes) * 100), 2)
-    p_Doane = round(((votes_Doane / total_votes) * 100), 2)
 
-    print ("Election Results")
-    print("---------------------------------------------")
-    print("Total Votes: ", total_votes)                
-    print(f"Stockham: %{p_Stockham} ({votes_Stockham})")
-    print(f"DeGette: %{p_DeGette} ({votes_Degette})")
-    print(f"Doane: %{p_Doane} ({votes_Doane})")
+    #-------read for total_votes
+    for csvrow in csvreader:
+            total_votes.append(csvrow[1])
+            total_votes = len(total_votes)
 
-    # The percentage of votes each candidate won.
+    #-------validate total votes row
+    # print(total_votes)  
 
-    # The winner of the election based on popular vote.
-    def winner(candidates):
-        return max(set(candidates), key = candidates.count)
+        #Add to count, if value candidate is located in column [2]
+        # use conditon to assign to each candidate
+            if csvrow[2] == "Charles Casper Stockham":
+                Stockham += 1
+    #print(Stockham)
 
-    # print("Winner:", min_pl_change_date,"($", round(min_pl_change),")")
-    print(candidates)
+            elif csvrow[2] == "Diana DeGette":
+                DeGette +=1
+    #print(DeGette)
+
+            elif csvrow[2] == "Raymon Anthony Doane":
+                Doane +=1
+    #print(Doane)
+
+    #Canidate Calculation
+    Canidate_stockham = round((Stockham/total_votes)*100,3)
+    Canidate_degette = round((DeGette/total_votes)*100,3)
+    Canidate_doane = round((Doane/total_votes)*100,3)
+
+    #total_votes by candidate dictionary
+    Canidate_total_votes = {"Charles Casper Stockham": Stockham, "Diana DeGette": DeGette, "Raymon Anthony Doane": Doane }  
+
+    winner = max(Canidate_total_votes, key=Canidate_total_votes.get)
+    #print(winner)   
+
+
+#Write to text file
+output_path = "election_result.txt"
+
+file =  open(output_path, 'w') 
+
+
+file.write("Election Results\n")
+file.write("----------------------------\n")
+file.write(f"Total total_votes: {total_votes}\n")
+file.write("----------------------------\n")
+file.write(f"Charles Casper Stockham: {Canidate_stockham}% ({Stockham})\n")
+file.write(f"Diana DeGette: {Canidate_degette}% ({DeGette})\n")
+file.write(f"Raymon Anthony Doane: {Canidate_doane}% ({Doane}) \n")
+file.write("----------------------------\n")
+file.write(f"Winner: {winner}\n")
+
+file.close()
